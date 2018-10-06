7643bd52cd853011af8c18dc85b7cf1701a23995
abhishekavi4602 <abhishekavi4602@gmail.com>
2018-10-06 09:28:34 +0530

    Note taking App

--- app/src/main/java/com/avi/abhishek/notetakingapp/MainActivity.java
+++ app/src/main/java/com/avi/abhishek/notetakingapp/MainActivity.java
@@ -1,8 +1,10 @@
 package com.avi.abhishek.notetakingapp;
 
+import android.content.Context;
 import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -38,14 +40,8 @@ public class MainActivity extends AppCompatActivity {
         ListView listView;
         listView = findViewById(R.id.listView);
 
-        SharedPreferences sharedPreferences= getSharedPreferences("Preferences",MODE_PRIVATE);
-        notes.clear();
-        int size= sharedPreferences.getInt("Status_size",0);
-        for(int i=0;i<size;i++)
-        {
-            notes.add(SharedPreferences.getString("Status_" + i, null));
-        }
 
+        notes= loadArray(this);
 
         final NotesAdapter notesAdapter = new NotesAdapter(notes);
 
@@ -72,18 +68,49 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onStop() {
 
-        SharedPreferences sharedPreferences = getSharedPreferences("Preferences",MODE_PRIVATE);
-        SharedPreferences.Editor editor = sharedPreferences.edit();
-
-        editor.putInt("Status_size", notes.size());
-
-        for(int i=0;i<notes.size();i++)
-        {
-            editor.remove("Status_" + i);
-            editor.putString("Status_" + i, "" +notes.get(i));
-        }
-        editor.apply();
+        saveArray();
         super.onStop();
+    }
+
+
+
+    public void saveArray() {
+        SharedPreferences sp = getSharedPreferences("my_pref", MODE_PRIVATE);
+        SharedPreferences.Editor mEdit1 = sp.edit();
+
+        mEdit1.putInt("Status_size", notes.size());
+
+        for (int i = 0; i < notes.size(); i++) {
+            //  mEdit1.remove("Status_" + i);
+            mEdit1.putString("Note_Message"+i , String.valueOf(notes.get(i).getTitle()));
+            mEdit1.putString("Note_Time"+i , String.valueOf(notes.get(i).getTime()));
+        }
+
+        mEdit1.apply();
+    }
+
+
+    public ArrayList<Note> loadArray(Context mContext) {
+//        SharedPreferences mSharedPreference1 =   PreferenceManager.getDefaultSharedPreferences(mContext);
+
+        ArrayList<Note> notesArrayList =new ArrayList<>();
+        SharedPreferences mSharedPreference1 = getSharedPreferences("my_pref", MODE_PRIVATE);
+        notes.clear();
+        int size = mSharedPreference1.getInt("Status_size", 0);
+
+        for (int i = 0; i < size; i++) {
+            String notes_mesg = mSharedPreference1.getString("Note_Message"+i, null);
+            String notes_time = mSharedPreference1.getString("Note_Time"+i, null);
+
+            Log.e("TAG", "loadArray: ---------"+notes_mesg+"--"+notes_time);
+            Log.e("TAG", "loadArray111111: ********"+notesArrayList.size() );
+
+            notesArrayList.add(new Note(notes_mesg, notes_time));
+
+            Log.e("TAG", "loadArray222222: ********"+notesArrayList.size() );
+        }
+
+        return notesArrayList;
     }
 }
 
